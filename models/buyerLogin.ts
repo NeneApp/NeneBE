e3718f6505eae3dafbd63862e31cf7e089bd42f4
Joshua thomas <thomasjoshuaseun@gmail.com>
2023-03-18 07:06:43 +0100

    feat(BAC-2): Created buyer login database schema

---
+++ models/buyerLogin.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from 'mongoose';
+
+export interface IBuyerLogin extends Document {
+    username: string;
+    email: string;
+    password: string;
+}
+
+const userSchema: Schema = new mongoose.Schema<IBuyerLogin>({
+  username: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 255,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: /^[a-zA-Z0-9_-]+$/
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    maxlength: 255,
+    lowercase: true,
+    trim: true,
+    match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 255,
+    trim: true,
+    match: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]+$/,
+    select: false
+  }
+});
+
+const BuyerModel = mongoose.model<IBuyerLogin>('Buyer', userSchema);
+
+export default BuyerModel;
