dd2812ed1f4ff4024e0f643b92837bb7f90144c2
Oluseyi Adeegbe <adeegbeoluseyi@gmail.com>
2023-03-23 01:33:04 +0100

    fix: removed verification link hard-coding and enviroment variable not loading properly

--- utility/MailerUtility.ts
+++ utility/MailerUtility.ts
@@ -1,12 +1,13 @@
 import nodemailer from 'nodemailer';
-
-
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const transport = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
   port: 587,
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.AUTH_EMAIL,
     pass: process.env.AUTH_PASS,
@@ -16,7 +17,8 @@ const transport = nodemailer.createTransport({
 export const sendConfirmationEmail = async (
   name: string,
   email: string,
-  confirmationCode: string
+  confirmationCode: string,
+  userType: string,
 ) => {
   try {
     return await transport.sendMail({
@@ -26,8 +28,7 @@ export const sendConfirmationEmail = async (
       html: `<h1>Email Confirmation</h1>
           <h2>Hello ${name}</h2>
           <p>Verify your email address to complete the signup and login to your account</p>
-          <a href=http://localhost:8000/api/vendors/confirm/${confirmationCode}> Click here</a>
-          </div>`,
+          <a href=${process.env.BASE_URL}/api/${userType}/confirm/${confirmationCode}> Click here</a>`,
     });
   } catch (error: any) {
     throw new Error(error.message);
