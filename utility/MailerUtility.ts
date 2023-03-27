f3dae3d1770225db58c29af89d67001d286b1aca
AwaabSingh <adeoyin2828@gmail.com>
2023-03-27 20:09:47 +0100

    send-mail

--- utility/MailerUtility.ts
+++ utility/MailerUtility.ts
@@ -2,6 +2,7 @@ import nodemailer from 'nodemailer';
 import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
+import config from 'config';
 
 const transport = nodemailer.createTransport({
   service: 'gmail',
@@ -17,20 +18,17 @@ const transport = nodemailer.createTransport({
 export const sendConfirmationEmail = async (
   name: string,
   email: string,
-  confirmationCode: string,
-  userType: string,
+  subject: string,
+  message: string
 ) => {
   try {
     return await transport.sendMail({
       from: process.env.AUTH_EMAIL,
       to: email,
-      subject: 'Please confirm your account',
-      html: `<h1>Email Confirmation</h1>
-          <h2>Hello ${name}</h2>
-          <p>Verify your email address to complete the signup and login to your account</p>
-          <a href=${process.env.BASE_URL}/api/${userType}/confirm/${confirmationCode}> Click here</a>`,
+      subject,
+      html: message,
     });
   } catch (error: any) {
-    throw new Error(error.message);
+    console.log(error.message);
   }
 };
