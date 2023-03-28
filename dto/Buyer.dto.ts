0b1f42fd1acc1a93bb0f46d40f77a355e8b5a2ea
Joshua thomas <thomasjoshuaseun@gmail.com>
2023-03-28 07:48:53 +0100

    Added zod input validation

--- dto/Buyer.dto.ts
+++ dto/Buyer.dto.ts
@@ -1,3 +1,5 @@
+import { object, string, TypeOf } from 'zod';
+
 export interface IBuyerRegisterInput {
   firstName: string;
   lastName: string;
@@ -13,3 +15,15 @@ export interface IBuyerUpdateInput {
   address?: string;
 }
 
+export const buyerLoginInputSchema = object({
+  body: object({
+    email: string({
+      required_error: 'Email is required',
+    }).email('Not a valid email address'),
+    password: string({
+      required_error: 'Password is required',
+    }).min(6, 'Password too short - should be atleast 8 characters'),
+  }),
+});
+
+export type IbuyerLoginInputSchema = TypeOf<typeof buyerLoginInputSchema>;
